4ebfe8e4c1c0d7942e4a7f3d7645746772b52d60
sergio.gallegos <sergio.gallegos@live.com.mx>
2025-08-05 22:43:24 -0600

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,6 +14,6 @@ const appRoutes: Routes = [
   ];
   
   export const AppRoutes = RouterModule.forRoot(appRoutes, {
-    useHash: true,
+    useHash: false,
     anchorScrolling: 'enabled',
     preloadingStrategy: PreloadAllModules});
